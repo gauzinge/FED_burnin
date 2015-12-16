d2457753b378c23252b46b71e135e7bdce7e5c2f
Georg Auzinger <georg.auzinger@cern.ch>
2015-12-16 11:01:01 +0100

    added script to copy logfiles to www directory on /afs

---
+++ copy_logfile.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+PATH=/usr/kerberos/bin/:/usr/bin:$PATH
+export PATH
+
+curr_principal=`klist 2>/dev/null|egrep "Default principal" |awk '{print $3}'`
+if [ "x$curr_principal" != "x" ]
+then
+    kinit -R
+    aklog
+fi
+echo 'Copying files for webpage!'
+export DIRNAME="/afs/cern.ch/user/g/gauzinge/www/cratemonitor/"
+echo 'moving files to' $DIRNAME
+cp /home/fectest/FED_testing/FED_burnin/*.txt $DIRNAME

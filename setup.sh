1c1fb123350b282bc9204dac570e07b98d005aa2
akornmay <andreaskornmayer@googlemail.com>
2016-02-18 14:23:09 +0100

    compiler and root are sourced from afs now

--- setup.sh
+++ setup.sh
@@ -27,8 +27,6 @@ export BOOST_INCLUDE=/opt/cactus/include
 #export QMAKESPEC=
 #export QTROOTSYSDIR=/usr/local/qtRoot/root
 
-#ROOT
-#source /usr/local/bin/thisroot.sh
 
 #FED Burnin
 export BASE_DIR=$(pwd)
@@ -36,3 +34,11 @@ export BASE_DIR=$(pwd)
 export PATH=$BASE_DIR/bin:$PATH
 export LD_LIBRARY_PATH=$CACTUSLIB:$BASE_DIR/lib:${LD_LIBRARY_PATH}
 export PATH=/usr/bin:/usr/local/bin:~/bin:${CACTUSBIN}:${AMC13BIN}:$PATH
+
+
+#GCC compiler
+
+. /afs/cern.ch/sw/lcg/external/gcc/4.8.4/x86_64-slc6/setup.sh
+
+#ROOT
+. /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.21/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh

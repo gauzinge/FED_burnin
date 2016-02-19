6f750bf6974668f73bb94c569e223c06b80d65fb
Georg Auzinger <schurl@pothead.home>
2016-02-19 09:49:24 +0100

    made new binary to test tkFEC parsing

---
+++ src/tkfectest.cc
@@ -0,0 +1,51 @@
+#include <cstring>
+#include "../Utils/Utilities.h"
+//#include "../HWInterface/PixFEDInterface.h"
+//#include "../HWDescription/PixFED.h"
+//#include "../HWInterface/FpgaConfig.h"
+#include "../Utils/Timer.h"
+//#include <inttypes.h>
+#include "../Utils/argvparser.h"
+#include "../Utils/ConsoleColor.h"
+//#include "../System/TkFECController.h"
+#include "../TkFED/TkFECController.h"
+
+
+
+using namespace CommandLineProcessing;
+using namespace std;
+
+
+
+int main( int argc, char* argv[] )
+{
+
+
+    TkFECController cTkFECController;
+    ArgvParser cmd;
+
+    // init
+    cmd.setIntroductoryDescription( "Ph1 TkFEC Configuration tool" );
+    // error codes
+    cmd.addErrorCode( 0, "Success" );
+    cmd.addErrorCode( 1, "Error" );
+    // options
+    cmd.setHelpOption( "h", "help", "Print this help page" );
+
+    cmd.defineOption( "interactive", "Run FEC SW Wrapper in Interactive mode." );
+    cmd.defineOptionAlternative( "interactive", "i" );
+
+    cmd.defineOption( "file", "HWDescription file" );
+    cmd.defineOptionAlternative( "file", "f" );
+
+    int result = cmd.parse( argc, argv );
+
+    if ( result != ArgvParser::NoParserError )
+    {
+        std::cout << cmd.parseErrorDescription( result );
+        exit( 1 );
+    }
+    std::string cHWFile = ( cmd.foundOption( "file" ) ) ? cmd.optionValue( "file" ) : "settings/HwDescription.xml";
+    cTkFECController.InitializeHw( cHWFile );
+
+}

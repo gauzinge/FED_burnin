3210d8490c65efcec6ac682b2f1e0daa5c8ee0ae
Georg Auzinger <georg.auzinger@cern.ch>
2016-05-25 12:22:22 +0200

    updated FEDconfig.cc, address table & HWDescription

--- src/fedconfig.cc
+++ src/fedconfig.cc
@@ -80,9 +80,9 @@ int main(int argc, char* argv[] )
             cSystemController.fFEDInterface->WriteBoardReg(cFED, "fe_ctrl_regs.decode_reg_reset", 1);
              mypause();
              //cSystemController.fFEDInterface->readTransparentFIFO(cFED);
-             cSystemController.fFEDInterface->readSpyFIFO(cFED);
+             //cSystemController.fFEDInterface->readSpyFIFO(cFED);
              cSystemController.fFEDInterface->readFIFO1(cFED);
-             cSystemController.fFEDInterface->readOSDWord(cFED, cROCOfInterest, cChannelOfInterest);
+             //cSystemController.fFEDInterface->readOSDWord(cFED, cROCOfInterest, cChannelOfInterest);
              //cSystemController.fFEDInterface->ReadData(cFED, 0 );
              cSystemController.fFEDInterface->ReadNEvents(cFED, cNEventsCommMode );
          }
